88c7bb5fe66ac33696327782e77fa2f7c4e738d7
shivam <shivamsoni.soni3@gmail.com>
2020-04-24 02:16:51 +0530

    Add: Restaurant activity is fully functional

--- app/src/main/java/com/example/tour_guide_21/Restaurents.java
+++ app/src/main/java/com/example/tour_guide_21/Restaurents.java
@@ -4,8 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 
 /**
@@ -24,6 +28,20 @@ public class Restaurents extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_restaurents, container, false);
+        ArrayList<Details> detailR = new ArrayList<Details>();
+        detailR.add(new Details(getString(R.string.Shahnama), getString(R.string.ShahnamaDes), R.drawable.shahnama));
+        detailR.add(new Details(getString(R.string.Sagffc), getString(R.string.SagffcDes), R.drawable.sagar_gair));
+        detailR.add(new Details(getString(R.string.Sharma), getString(R.string.ShahnamaDes), R.drawable.sharma));
+        detailR.add(new Details(getString(R.string.Raw), getString(R.string.Rawdes), R.drawable.res_1));
+        detailR.add(new Details(getString(R.string.Underm), getString(R.string.UndermDes), R.drawable.charming));
+        detailR.add(new Details(getString(R.string.Bapu), getString(R.string.BapuDes), R.drawable.bapu));
+
+
+        DetailsAdapter detailsAdapterR = new DetailsAdapter(Objects.requireNonNull(getActivity()), detailR);
+        ListView listViewR = view.findViewById(R.id.list_res);
+        assert listViewR != null;
+        if (listViewR != null)
+            listViewR.setAdapter(detailsAdapterR);
         return view;
     }
 }
